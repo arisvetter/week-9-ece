9d5ac4fec02860abaeef017e19dda16dec6d8eb7
ttylercasey <156113672+ttylercasey@users.noreply.github.com>
2025-10-21 11:30:03 -0600

    Update demux.v

--- class (2) 2/demux.v
+++ class (2) 2/demux.v
@@ -1,15 +1,15 @@
 module demux(
     input [3:0] UserData,
     input [1:0] Sel,
-    output [1:0] localLib,
-    output [1:0] fireDep,
-    output [1:0] school,
-    output [1:0] ribShack
+    output [3:0] localLib,
+    output [3:0] fireDep,
+    output [3:0] school,
+    output [3:0] ribShack
 );
 
-    assign localLib = (Sel == 'b00 ? UserData : 0000); // Drive Y1 if Sel == 0
-    assign fireDep = (Sel == 'b01 ? UserData : 0000); // Drive Y2 if Sel == 1
-    assign school = (Sel == 'b10 ? UserData : 0000); // Drive Y3 if Sel == 0
-    assign ribShack = (Sel == 'b11 ? UserData : 0000); // Drive Y4 if Sel == 1
+    assign localLib = (Sel == 'b00 ? UserData : 'b0000); // Drive Y1 if Sel == 0
+    assign fireDep = (Sel == 'b01 ? UserData : 'b0000); // Drive Y2 if Sel == 1
+    assign school = (Sel == 'b10 ? UserData : 'b0000); // Drive Y3 if Sel == 0
+    assign ribShack = (Sel == 'b11 ? UserData : 'b0000); // Drive Y4 if Sel == 1
 
 endmodule
