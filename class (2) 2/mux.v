0a7ad97caccb578e33f1cc85e84abacc104aad93
ttylercasey <156113672+ttylercasey@users.noreply.github.com>
2025-10-21 11:29:40 -0600

    Update mux.v

--- class (2) 2/mux.v
+++ class (2) 2/mux.v
@@ -1,12 +1,10 @@
-module simple_4_mux(
-    input [1:0] Sel,
-    input [3:0] CEOData, [3:0] YourData, [3:0] FredData, [3:0] JillData, Enable,
-    output [3:0] Y,
-);
 
-    assign Y = Enable? (
-        (~Sel[0] & ~Sel[1])? CEOdata: (Sel[0] & ~Sel[1])? YourData: (~Sel[0] &  Sel[1])? FredData :  (Sel[0] &  Sel[1])? JillData: 0000
-        
-        ) : 0000;
+module mux(
+    input [1:0] Sel,
+    input [3:0] CEOData, YourData, FredData, JillData, 
+    input Enable,
+    output [3:0] UserData
+    );
+    assign UserData = Enable? ((~Sel[0] & ~Sel[1])? CEOData: (Sel[0] & ~Sel[1])? YourData: (~Sel[0] &  Sel[1])? FredData : (Sel[0] &  Sel[1])? JillData:0000) : 0000;
 
 endmodule
