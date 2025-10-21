ff5fb2f6833067cdd6e93ac96dc2d12db337a2b7
arisvetter <142842842+arisvetter@users.noreply.github.com>
2025-10-21 15:51:09 +0000

    demux

--- class (2) 2/demux.v
+++ class (2) 2/demux.v
@@ -1,15 +1,15 @@
 module demux(
-    input [1:0] In,
-    input Sel,
-    output [1:0] Y1,
-    output [1:0] Y2,
-    output [1:0] Y3,
-    output [1:0] Y4
+    input [3:0] UserData,
+    input [1:0] Sel,
+    output [1:0] localLib,
+    output [1:0] fireDep,
+    output [1:0] school,
+    output [1:0] ribShack
 );
 
-    assign Y1 = (Sel == 1'b0 ? In : 0); // Drive Y1 if Sel == 0
-    assign Y2 = (Sel == 1'b1 ? In : 0); // Drive Y2 if Sel == 1
-    assign Y3 = (Sel == 1'b2 ? In : 0); // Drive Y1 if Sel == 0
-    assign Y4 = (Sel == 1'b3 ? In : 0); // Drive Y2 if Sel == 1
+    assign localLib = (Sel == 1'b0 ? In : 0000); // Drive Y1 if Sel == 0
+    assign fireDep = (Sel == 1'b1 ? In : 0000); // Drive Y2 if Sel == 1
+    assign school = (Sel == 1'b2 ? In : 0000); // Drive Y3 if Sel == 0
+    assign ribShack = (Sel == 1'b3 ? In : 0000); // Drive Y4 if Sel == 1
 
 endmodule
